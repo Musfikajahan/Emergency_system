605a76bcea102e4e4fc6c91e52ad193ac8432912
Musfikajahan <musfikajahanmithun7117@gmail.com>
2025-08-29 03:54:46 +0600

    third

---
+++ script.js
@@ -0,0 +1,453 @@
+Musfika jahan
+musfikajahan
+Online
+
+Musfika jahan — 6/5/25, 4:51 PM
+Image
+Musfika jahan — 6/10/25, 8:27 PM
+In recognition of outstanding performance, exceptional reflexes, and unwavering dedication as the Goalkeeper during the football tournament.
+Musfika jahan — 6/30/25, 12:32 AM
+https://form.10ms.io/r/w4RMx5?fbclid=IwY2xjawLOZexleHRuA2FlbQIxMQABHro5jlEJ1p_1dPIoHDi1Wm2A1XMev02unlBsphH2j4W9OTAJXhuCQImirioh_aem_Wkhj4jt9JAjA9aNCSPxy_w
+form.10ms.io
+Assistant Teacher
+Assistant Teacher
+Musfika jahan — 6/30/25, 12:53 AM
+https://drive.google.com/file/d/1dZOJW-pY9T_qYRdHAqZMzMccTQKruUsb/view?usp=drivesdk
+https://www.facebook.com/share/1AyQSRQDxv/
+Log in or sign up to view
+See posts, photos and more on Facebook.
+Musfika jahan — 6/30/25, 1:14 AM
+My first experience as a teacher was incredibly special because it happened at Bhai Bon Model School,Bogura,where I began my academic journey.But this time as an educator, was truly nostalgic and emotional for me.I began teaching in 2019. In this role, I was responsible for student support and curriculum delivery, working closely with students to ensure their academic success. When the COVID-19 pandemic struck and education shifted online, I adapted by conducting classes through Zoom. During this time, I also served on the examination board, contributing to academic assessments and evaluation processes.
+Overall, this experience helped shape my confidence, communication skills  and commitment to education. It remains a foundational chapter in my career as an educator.
+Attachment file type: acrobat
+MusfikajahanCV (2).pdf
+168.96 KB
+Md.Nazmul Hasan — 6/30/25, 1:23 AM
+https://drive.google.com/drive/folders/1IAgCqqmoofAcm0gtgi0TRAhOEr1hkco1?usp=drive_link
+Google Drive
+Md.Nazmul Hasan — 6/30/25, 2:45 AM
+check de to
+Attachment file type: acrobat
+MD.-NAZMUL-HASAN-FlowCV-Resume-20250630.pdf
+92.07 KB
+Musfika jahan — 6/30/25, 2:50 AM
+Ekkere makkhon
+Md.Nazmul Hasan — 6/30/25, 2:51 AM
+khara oi paragraph ta krsi just paraphrase kore dissi
+Musfika jahan — 6/30/25, 2:51 AM
+okkkkkkkk
+Md.Nazmul Hasan — 6/30/25, 2:53 AM
+I will always remember the first time I taught. It happened at the MS Academy, which is where I started to really shine as a teacher. When I went back as a teacher this time, I felt filled with memories and pain. I started working as a teacher in 2024, and my job needed me to both help the students and come up with fun ways to teach the material. Building relationships with students was fun for me. I liked being able to teach them and give them advice online over Zoom, and I liked being able to quickly and safely adapt my skills to their needs. It has given me more confidence, helped me improve my speaking skills, sparked my desire to teach, and turned this time of growth into an important part of my life as a teacher.
+check
+Musfika jahan — 6/30/25, 2:54 AM
+thik aseeeeeeeeeeeeeeeeeeeeee
+Md.Nazmul Hasan — 6/30/25, 2:54 AM
+cv te ki image dite hbe
+Musfika jahan — 7/5/25, 10:33 PM
+https://drive.google.com/file/d/1gNW100CQ_Vi5iAbRyyWkGpaehpSJOMAn/view?usp=drivesdk
+Google Docs
+FinalDemo.mp4
+Musfika jahan — 7/11/25, 8:01 PM
+https://www.youtube.com/playlist?list=PLNDrhDXd0Qt8C8J4im9MV200WIOqlyZkx
+YouTube
+CSE420 Compiler Bracu
+Image
+Musfika jahan — 7/12/25, 12:41 AM
+https://www.youtube.com/watch?v=mpear29GpG4
+YouTube
+Farhan Hossan
+Lec: 18 | LR(0) Parsing Table | Compiler Design | Bangla Tutorial
+Image
+Musfika jahan — 7/12/25, 10:14 PM
+Attachment file type: acrobat
+idcard.pdf
+364.00 KB
+Md.Nazmul Hasan — 7/19/25, 8:18 PM
+https://www.youtube.com/watch?v=sMxqUQc_jHQ
+YouTube
+Gate Smashers
+Lec-13: CLR Parsing Table | LR(1) Canonical Items
+Image
+Musfika jahan — 7/19/25, 11:42 PM
+Attachment file type: acrobat
+24341134-Musfika Jahan.pdf
+6.26 MB
+Musfika jahan — 7/20/25, 9:21 PM
+https://www.cineplexbd.com/
+Cineplex Web
+Bangladesh's most popular destination for movies, showtimes, tickets, and trailers.
+Md.Nazmul Hasan — 7/23/25, 7:19 PM
+Image
+Image
+Image
+Image
+https://www.perplexity.ai/search/i-want-to-give-you-some-econom-CsX_GOBvSTu.7P7MJDBmIw
+Perplexity AI
+i want to give you some economics pdf which you need to study and g...
+I understand your request. Please go ahead and provide the economics PDF you want me to study. Once I've reviewed it, I will help you solve your assignment...
+i want to give you some economics pdf which you need to study and g...
+Musfika jahan — 7/23/25, 10:38 PM
+Image
+middle ta a
+middle ta b
+Image
+Musfika jahan — 7/25/25, 2:09 PM
+Attachment file type: archive
+flower boquet.zip
+5.95 MB
+Musfika jahan — 7/26/25, 2:21 PM
+E →  E + T
+E → T
+T → T * F
+T → F
+F → ( E )
+F → id
+Image
+Musfika jahan — 7/26/25, 7:09 PM
+eta bujcis oc kmne ber korse
+Image
+Musfika jahan — 7/28/25, 12:05 PM
+https://drive.google.com/drive/mobile/folders/1hJtpQt0uuNCVLLDvWkzsFh-XPDPAQET3?fbclid=IwY2xjawLzERpleHRuA2FlbQIxMABicmlkETFOSHpRVHJrNWs0Y2Y1Y2xzAR732y93S6LZvulgoU79oM1aoyxGIW4aNxsBZ71KZENGIjJOFkMceCr8HoYjiw_aem_z_oD7Wj4IscBxlqCqvslWg
+Google Drive
+Musfika jahan — 7/28/25, 8:25 PM
+https://www.perplexity.ai/search/explain-easily-z3ThMFuyR8uvw.Sjg3TjIQ
+Perplexity AI
+explain easily
+Sure! Here’s an easy explanation of the content in the image:
+
+1. How SDT is implemented:
+- You can implement any SDT by first building a parse tree (a...
+explain easily
+Md.Nazmul Hasan — 8/2/25, 12:32 AM
+https://www.youtube.com/watch?v=JdwE-8gCwQg&list=WL&index=2
+YouTube
+Sakib Hasan Simanto
+CSE437 DS -  Random Forest With Gini Impurity (Midterm Prep)
+Image
+Md.Nazmul Hasan — 8/2/25, 2:20 PM
+https://drive.google.com/drive/folders/1u0bAR6kEyI9JVhUAezTPs3fcfJZ1oeyU
+Google Drive
+Musfika jahan — 8/2/25, 2:27 PM
+https://www.youtube.com/watch?v=xnfiLm6GtFY
+YouTube
+Mahesh Huddar
+Build Decision Tree using Regression Algorithm  Solved Example in m...
+Image
+Musfika jahan — 8/2/25, 8:15 PM
+https://discord.gg/SedKCtaP
+Musfika jahan — 8/2/25, 8:24 PM
+Image
+Musfika jahan — 8/2/25, 10:25 PM
+Image
+Image
+Musfika jahan — 8/2/25, 10:59 PM
+Image
+Musfika jahan — 8/4/25, 11:02 PM
+Attachment file type: archive
+Assignment3.zip
+1.98 KB
+Md.Nazmul Hasan — 8/4/25, 11:32 PM
+Attachment file type: archive
+Assignment-3.zip
+1.84 KB
+Musfika jahan — 8/9/25, 11:04 PM
+https://meet.google.com/yym-xrru-yoe
+Meet
+Real-time meetings by Google. Using your browser, share your video, desktop, and presentations with teammates and customers.
+Image
+Musfika jahan — 8/14/25, 11:02 PM
+Attachment file type: archive
+Assignment4.zip
+1.31 KB
+Md.Nazmul Hasan — 8/15/25, 1:19 AM
+Attachment file type: archive
+assignment.zip
+17.57 KB
+Musfika jahan — 8/20/25, 11:13 PM
+https://meet.google.com/dsy-wjsv-qrk
+Meet
+Real-time meetings by Google. Using your browser, share your video, desktop, and presentations with teammates and customers.
+Image
+Musfika jahan — Yesterday at 4:19 PM
+Can you please share this with any BRAC University students who might be interested? 🙂
+
+A family is looking for a Private Tutor for their 5-year-old child (Nursery student) at Chittagong Grammar School, Banani Campus.
+
+📍 Location: Road #23, Banani
+📅 Schedule: 5 days a week, 3:30/4:00 – 5:30/6:00 PM (2 hrs daily)
+📚 Subjects: English, Math, Bangla, Drawing & Coloring, Reading (English & Bangla)
+✨ Preference: BRACU student (preferably from nearby) with prior teaching experience
+
+🗓️ Deadline: Sat, 23 Aug 2025
+
+👉 Apply here (fill out form + upload CV):
+https://forms.gle/r75UQBcRAc6US6Vb8
+
+@everyone please circulate among your clubs. And apply if you are interested.
+Md.Nazmul Hasan — 11:30 PM
+<main class="flex flex-row gap-4 px-4 py-6 bg-gray-50 max-w-screen-lg mx-auto">
+
+  <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 mb-4">
+    <div class="bg-white rounded-lg p-4 shadow-md flex flex-col">
+      <div class="flex items-center justify-between mb-3">
+        <div class="text-green-600 text-3xl">🚨</div>
+Expand
+message.txt
+15 KB
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Emergency Hotline</title>
+Expand
+index.html
+18 KB
+@import 'tailwindcss/base';
+@import 'tailwindcss/components';
+@import 'tailwindcss/utilities';
+tailwind.config.css
+1 KB
+Musfika jahan — 1:01 AM
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Emergency Hotline</title>
+Expand
+message.txt
+18 KB
+Musfika jahan — 2:17 AM
+Image
+index.global.js:896 The browser build of Tailwind CSS should not be used in production. To use Tailwind CSS in production, use the Tailwind CLI, Vite plugin, or PostCSS plugin: https://tailwindcss.com/docs/installation
+(anonymous) @ index.global.js:896
+(anonymous) @ index.global.js:1
+index.html:1 Refused to apply style from 'http://127.0.0.1:5500/style.css' because its MIME type ('text/html') is not a supported stylesheet MIME type, and strict MIME checking is enabled.
+index.html:1 Refused to apply style from 'http://127.0.0.1:5500/tailwindcss/components' because its MIME type ('text/html') is not a supported stylesheet MIME type, and strict MIME checking is enabled.
+index.html:1 Refused to apply style from 'http://127.0.0.1:5500/tailwindcss/base' because its MIME type ('text/html') is not a supported stylesheet MIME type, and strict MIME checking is enabled.
+index.html:1 Refused to apply style from 'http://127.0.0.1:5500/tailwindcss/utilities' because its MIME type ('text/html') is not a supported stylesheet MIME type, and strict MIME checking is enabled.
+index.html:289  GET http://127.0.0.1:5500/script.js/ net::ERR_ABORTED 404 (Not Found)
+index.html:1 Refused to execute script from 'http://127.0.0.1:5500/script.js/' because its MIME type ('text/html') is not executable, and strict MIME type checking is enabled.
+Installing with Vite - Installation
+Integrate Tailwind CSS with frameworks like Laravel, SvelteKit, React Router, and SolidJS.
+Installing with Vite - Installation
+Musfika jahan — 2:24 AM
+index.global.js:896 The browser build of Tailwind CSS should not be used in production. To use Tailwind CSS in production, use the Tailwind CLI, Vite plugin, or PostCSS plugin: https://tailwindcss.com/docs/installation
+(anonymous) @ index.global.js:896
+(anonymous) @ index.global.js:1
+index.html:289  GET http://127.0.0.1:5500/script.js/ net::ERR_ABORTED 404 (Not Found)
+index.html:1 Refused to execute script from 'http://127.0.0.1:5500/script.js/' because its MIME type ('text/html') is not executable, and strict MIME type checking is enabled.
+Installing with Vite - Installation
+Integrate Tailwind CSS with frameworks like Laravel, SvelteKit, React Router, and SolidJS.
+Installing with Vite - Installation
+Md.Nazmul Hasan — 3:54 AM
+// button
+
+document.getElementById('buttonP').addEventListener('click', function(event){
+   event.preventDefault();
+   const heartCount = parseInt(document.getElementById('heart-count').innerText) || 0;
+   document.getElementById('heart-count').innerText = heartCount % 2 === 1 ? heartCount + 1 : heartCount + 1;
+Expand
+message.txt
+7 KB
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Emergency Hotline</title>
+Expand
+message.txt
+15 KB
+﻿
+Md.Nazmul Hasan
+nazmul.hasan
+ 
+// button
+
+document.getElementById('buttonP').addEventListener('click', function(event){
+   event.preventDefault();
+   const heartCount = parseInt(document.getElementById('heart-count').innerText) || 0;
+   document.getElementById('heart-count').innerText = heartCount % 2 === 1 ? heartCount + 1 : heartCount + 1;
+   document.getElementById('heart-img').src = heartCount % 2 === 1 ? 'assets/heart-filled.png' : 'assets/heart.png';
+} )
+
+
+
+// coin reduction
+
+document.getElementById('call-btn').addEventListener('click', function(){
+    const coinCount = parseInt(document.getElementById('coin-count').innerText) || 0;
+    document.getElementById('coin-count').innerText = coinCount - 20;
+
+    if(coinCount >= 20){
+        if(document.getElementById('call-btn').id === 'call-btn')
+        alert("Calling emmergency service 999.....");
+    }
+
+    if(coinCount <=0){
+        document.getElementById('coin-count').innerText = 0;
+        alert("Not sufficient coin");
+    }
+
+});
+// police
+document.getElementById('police').addEventListener('click', function(){
+    const coinCount = parseInt(document.getElementById('coin-count').innerText) || 0;
+    document.getElementById('coin-count').innerText = coinCount - 20;
+
+    if(coinCount >= 20){
+        if(document.getElementById('police').id === 'police')
+        alert("Calling police helpline 999.....");
+    }
+
+    if(coinCount <=0){
+        document.getElementById('coin-count').innerText = 0;
+        alert("Not sufficient coin");
+    }
+
+});
+// fire service
+document.getElementById('fire').addEventListener('click', function(){
+    const coinCount = parseInt(document.getElementById('coin-count').innerText) || 0;
+    document.getElementById('coin-count').innerText = coinCount - 20;
+
+    if(coinCount >= 20){
+        if(document.getElementById('fire').id === 'fire')
+        alert("Calling emergency service 999.....");
+    }
+
+    if(coinCount <=0){
+        document.getElementById('coin-count').innerText = 0;
+        alert("Not sufficient coin");
+    }
+
+});
+// ambulance
+document.getElementById('ambulance').addEventListener('click', function(){
+    const coinCount = parseInt(document.getElementById('coin-count').innerText) || 0;
+    document.getElementById('coin-count').innerText = coinCount - 20;
+
+    if(coinCount >= 20){
+        if(document.getElementById('ambulance').id === 'ambulance')
+        alert("Calling emergency service 1994-999999.....");
+    }
+
+    if(coinCount <=0){
+        document.getElementById('coin-count').innerText = 0;
+        alert("Not sufficient coin");
+    }
+
+});
+// women
+document.getElementById('women').addEventListener('click', function(){
+    const coinCount = parseInt(document.getElementById('coin-count').innerText) || 0;
+    document.getElementById('coin-count').innerText = coinCount - 20;
+
+    if(coinCount >= 20){
+        if(document.getElementById('women').id === 'women')
+        alert("Calling emergency service 109.....");
+    }
+
+    if(coinCount <=0){
+        document.getElementById('coin-count').innerText = 0;
+        alert("Not sufficient coin");
+    }
+
+});
+// govt
+document.getElementById('govt').addEventListener('click', function(){
+    const coinCount = parseInt(document.getElementById('coin-count').innerText) || 0;
+    document.getElementById('coin-count').innerText = coinCount - 20;
+
+    if(coinCount >= 20){
+        if(document.getElementById('govt').id === 'govt')
+        alert("Calling emergency service 106.....");
+    }
+
+    if(coinCount <=0){
+        document.getElementById('coin-count').innerText = 0;
+        alert("Not sufficient coin");
+    }
+
+});
+// current
+document.getElementById('current').addEventListener('click', function(){
+    const coinCount = parseInt(document.getElementById('coin-count').innerText) || 0;
+    document.getElementById('coin-count').innerText = coinCount - 20;
+
+    if(coinCount >= 20){
+        if(document.getElementById('current').id === 'current')
+        alert("Calling emergency service 16216.....");
+    }
+
+    if(coinCount <=0){
+        document.getElementById('coin-count').innerText = 0;
+        alert("Not sufficient coin");
+    }
+
+});
+// NGO
+document.getElementById('NGO').addEventListener('click', function(){
+    const coinCount = parseInt(document.getElementById('coin-count').innerText) || 0;
+    document.getElementById('coin-count').innerText = coinCount - 20;
+
+    if(coinCount >= 20){
+        if(document.getElementById('NGO').id === 'NGO')
+        alert("Calling emergency service 16445.....");
+    }
+
+    if(coinCount <=0){
+        document.getElementById('coin-count').innerText = 0;
+        alert("Not sufficient coin");
+    }
+
+});
+// Railway
+document.getElementById('railway').addEventListener('click', function(){
+    const coinCount = parseInt(document.getElementById('coin-count').innerText) || 0;
+    document.getElementById('coin-count').innerText = coinCount - 20;
+
+    if(coinCount >= 20){
+        if(document.getElementById('railway').id === 'railway')
+        alert("Calling emergency service 163.....");
+    }
+
+    if(coinCount <=0){
+        document.getElementById('coin-count').innerText = 0;
+        alert("Not sufficient coin");
+    }
+
+});
+
+
+// history
+
+document.getElementById('call-btn').addEventListener('click', function(){
+  const historyList = document.getElementById('call-history-list');
+
+  if(document.getElementById('call-btn').id === 'call-btn'){
+    const listItem = document.createElement('li');
+    const now = new Date();
+    const timeString = now.toLocaleTimeString('en-US', {
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+      hour12: true,
+      timeZoneName: 'short' 
+    });
+
+    listItem.innerHTML = `
+      <div style="display: flex; justify-content: space-between; align-items: center;">
+        <strong>Called emergency service</strong>
+        <span style="font-size: small; color: gray;">${timeString}</span>
+      </div>
+      <div>999</div>
+    `;
+
+    historyList.appendChild(listItem);
+  }
+});
+message.txt
+7 KB
